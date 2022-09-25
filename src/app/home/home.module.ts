16fca489883181fa4e1f3b178b6ddfe56ce686c7
Paulo Vargas <paulotomegomesdevargas@hotmail.com>
2022-09-25 11:19:23 -0300

    Ok

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,8 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { SquareComponent } from '../components/square/square.component';
+
 
 @NgModule({
   imports: [
@@ -14,6 +16,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, SquareComponent]
 })
 export class HomePageModule {}
